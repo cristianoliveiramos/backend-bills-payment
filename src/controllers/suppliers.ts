46cee61e7a77187c21ef6ddb61c79610614b7900
cristianoliveiramos <cristianoliveiramos@gmail.com>
2026-01-16 11:25:54 -0300

    Changed limit of rows and refactored indentation

--- src/controllers/suppliers.ts
+++ src/controllers/suppliers.ts
@@ -1,28 +1,30 @@
 import Suppliers from "../models/suppliers";
 
-export default app => {
-  console.log("Controller called")
+export default (app) => {
+  console.log("Controller called");
   app.get("/suppliers", (req, res) => Suppliers.list(res));
 
   app.post("/suppliers", (req, res) => {
-    const supplier = req.body;    
+    const supplier = req.body;
     console.log(req.body);
-    
+
     Suppliers.add(res, supplier);
-
-    
-
   });
 
   app.get("/supplier/:id", (req, res) => {
-    const id = parseInt(req.params.id)    
-    Suppliers.listById(id, res)
-  })
+    const id = parseInt(req.params.id);
+    Suppliers.listById(id, res);
+  });
 
   app.put("/supplier/:id", (req, res) => {
-    const id = parseInt(req.params.id)
-    const supplier = req.body
+    const id = parseInt(req.params.id);
+    const supplier = req.body;
 
-    Suppliers.update(id, res, supplier)
-  })
+    Suppliers.update(id, res, supplier);
+  });
+
+  app.delete("/supplier/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    Suppliers.delete(id, res);
+  });
 };
--- src/models/suppliers.ts
+++ src/models/suppliers.ts
@@ -2,62 +2,76 @@ import connection from "../database/connection";
 
 class Suppliers {
   list(res: any) {
-    const sql = "SELECT * FROM suppliers ORDER BY id";
-    
+    const sql = "SELECT * FROM suppliers ORDER BY id LIMIT 10 OFFSET 0";
+
     connection.query(sql, (error: any, result: any) => {
       if (error) {
         res.status(400).json(error);
         return;
       } else {
         res.status(200).json(result);
-        console.log('RESULT: ', result)
+        console.log("RESULT: ", result);
         return;
       }
     });
   }
 
   listById(id: number, res) {
-    const sql = `SELECT * FROM suppliers WHERE id=${id}`
-    console.log(typeof id)
+    const sql = `SELECT * FROM suppliers WHERE id=${id}`;
+
     connection.query(sql, (error: any, result: any) => {
-      const supplier = result[0]
-      if(error) {
-        res.status(400).json(error)
-        return
+      const supplier = result[0];
+      if (error) {
+        res.status(400).json(error);
+        return;
       } else {
-        res.status(200).json(supplier)
+        res.status(200).json(supplier);
         console.log(supplier);
-        return
+        return;
       }
-    })
+    });
   }
 
   add(res: any, supplier: any) {
     const sql = "INSERT INTO suppliers SET ?";
 
     connection.query(sql, supplier, (error: any, result: any) => {
-      if(error) {
-        res.status(400).json(error)
-        return
+      if (error) {
+        res.status(400).json(error);
+        return;
       } else {
-        res.status(200).json(result)
-        return
+        res.status(200).json(result);
+        return;
       }
-    })
+    });
   }
 
   update(id: number, res, values) {
-    const sql = `UPDATE suppliers SET ? WHERE id=${id}`
+    const sql = `UPDATE suppliers SET ? WHERE id=${id}`;
 
     connection.query(sql, [values, id], (error, result) => {
-      if(error) {
-        res.status(400).json(error)
-        return
+      if (error) {
+        res.status(400).json(error);
+        return;
       } else {
-        res.status(200).json(result)
-        return
+        res.status(200).json(result);
+        return;
       }
-    })
+    });
+  }
+
+  delete(id: number, res: any): void {
+    const sql = `DELETE FROM suppliers WHERE id=${id};`;
+    connection.query(sql, (error, result) => {
+      const supplier = result[0];
+      if (error) {
+        res.status(400).json(error);
+        return;
+      } else {
+        res.status(200).json(supplier);
+        return;
+      }
+    });
   }
 }
 
